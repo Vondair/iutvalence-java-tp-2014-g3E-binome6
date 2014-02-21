5d54670ae011e241e0e1a88ac86a3dd7fe43ef0b
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-21 23:50:45 +0100

    Add TODO (Puissance4.java)

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -1,12 +1,17 @@
 public class Puissance4 {
+    /* TODO Est-ce que cet attribut va changer en cours de partie ? final ? */
     /** Représente la grille de jeu. */
     public int[][] grille;
+    /* TODO Est-ce que cet attribut va changer en cours de partie ? final ? */
     /** Nom du joueur 1. */
     public String  J1;
+    /* TODO Est-ce que cet attribut va changer en cours de partie ? final ? */
     /** Nom du joueur 2. */
     public String  J2;
+    /* TODO Est-ce que cet attribut peut être différent de 1 ? constante ? */
     /** Pion du joueur 1. */
     public int pionJ1 = 1;
+    /* TODO Est-ce que cet attribut peut être différent de 1 ? constante ? */
     /** Pion du joueur 2. */
     public int pionJ2 = 2;
 
