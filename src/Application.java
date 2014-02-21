be4e2bf0ff36d89a995c5e99b5daef393f9e4737
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-21 23:46:29 +0100

    Javadoc refactoring.

--- src/Application.java
+++ src/Application.java
@@ -1,19 +1,15 @@
 /**
+ * "Launcher" du jeu.
+ *
  * @author carriean-cottenom
- * On crée la classe Application.
- * Cette classe sera le "launcher" du jeu
  */
 public class Application {
 
-	/**
-	 * @param args
-	 * Création de la méthode principale.
-	 */
-	public static void main(String[] args) {
-		Puissance4 P4 = new Puissance4("Tom","Jerry");
-		P4.jouer();
-	}
-
-	
-	
+    /**
+     * Méthode principale.
+     */
+    public static void main(String[] args) {
+        Puissance4 P4 = new Puissance4("Tom", "Jerry");
+        P4.jouer();
+    }
 }
