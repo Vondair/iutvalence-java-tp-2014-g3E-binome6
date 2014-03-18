13ee8fed564c3e2443c6020326343ed82825b032
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 13:36:03 +0100

    Add TODO (Pion.java)

--- src/Pion.java
+++ src/Pion.java
@@ -1,23 +1,25 @@
+/* TODO JavaDoc. */
+/* TODO Package. */
 
 public enum Pion {
-	CASE_VIDE(" ",0),
-	PION_J1("O",1),
-	PION_J2("X",2);
-	
-	private final String representation;
-	private final int valeurDuPion;
-	
-	private Pion(String repr, int pion) {
-		this.representation = repr;
-		this.valeurDuPion = pion;
-	}
-	
-	public int obtenirValeurDuPion() {
-		return this.valeurDuPion;
-	}
-	
-	@Override
-	public String toString() {
-		return representation;
-	}
+    CASE_VIDE(" ", 0),
+    PION_J1("O", 1),
+    PION_J2("X", 2);
+
+    private final String representation;
+    private final int    valeurDuPion;
+
+    Pion(String repr, int pion) {
+        this.representation = repr;
+        this.valeurDuPion = pion;
+    }
+
+    public int valeur() {
+        return this.valeurDuPion;
+    }
+
+    @Override
+    public String toString() {
+        return representation;
+    }
 }
