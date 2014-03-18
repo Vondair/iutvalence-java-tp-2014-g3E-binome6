382473368f621344d871ff0cd1c09a4678702e1b
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 13:35:45 +0100

    Add TODO (Application.java)

--- src/Application.java
+++ src/Application.java
@@ -1,13 +1,13 @@
+/* TODO Package. */
+
 /**
  * "Launcher" du jeu.
  * 
  * @author Antoine C, Marine C.
+ * @version TODO
  */
 public class Application {
-
-	/**
-	 * Méthode principale.
-	 */
+	/** Méthode principale. */
 	public static void main(String[] args) {
 		Puissance4 P4 = new Puissance4("Tom", "Jerry");
 		P4.jouer();
