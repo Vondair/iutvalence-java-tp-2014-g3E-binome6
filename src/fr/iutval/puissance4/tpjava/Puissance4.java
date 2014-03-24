afe38067f4e85977c070ce5c9330d04c543736e6
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-24 21:43:09 +0100

    Add comments, rename methods, add TODO.

--- src/fr/iutval/puissance4/tpjava/Puissance4.java
+++ src/fr/iutval/puissance4/tpjava/Puissance4.java
@@ -9,36 +9,36 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class Puissance4 {
-	/** Nombre de lignes */
+	/** Nombre de lignes. */
 	public static final int NB_LIGNES = 6;
-	/** Nombre de colonnes */
+	/** Nombre de colonnes. */
 	public static final int NB_COLONNES = 7;
 
     /** Premier joueur. */
-	private final Joueur J1;
+	private final Joueur joueur1;
 
     /** Second joueur. */
-	private final Joueur J2;
+    private final Joueur joueur2;
 
     /** Joueur courant. */
-	private Joueur joueurCourant;
+    private Joueur joueurCourant;
 
-    /** Représente la grille de jeu. */
-	private final Pion[][] grille;
+    /** Grille de jeu. */
+    private final Pion[][] grille;
 
 
-	/** Constructeur initialisant une grille vide et retenant le nom des joueurs. */
-	public Puissance4(String joueur1, String joueur2) {
-		this.grille = new Pion[Puissance4.NB_LIGNES][Puissance4.NB_COLONNES];
-		for (int i = 0; i < Puissance4.NB_LIGNES; i++) {
-			for (int j = 0; j < Puissance4.NB_COLONNES; j++) {
-				this.grille[i][j] = Pion.CASE_VIDE;
+    /** Constructeur initialisant une grille vide et retenant le nom des joueurs. */
+    public Puissance4(String joueur1, String joueur2) {
+        this.grille = new Pion[Puissance4.NB_LIGNES][Puissance4.NB_COLONNES];
+        for (int i = 0; i < Puissance4.NB_LIGNES; i++) {
+            for (int j = 0; j < Puissance4.NB_COLONNES; j++) {
+                this.grille[i][j] = Pion.CASE_VIDE;
             }
         }
 
-		this.J1 = new Joueur(joueur1, Pion.PION_J1);
-		this.J2 = new Joueur(joueur2, Pion.PION_J2);
-		this.joueurCourant = this.J1;
+        this.joueur1 = new Joueur(joueur1, Pion.PION_J1);
+		this.joueur2 = new Joueur(joueur2, Pion.PION_J2);
+		this.joueurCourant = this.joueur1;
 	}
 
 	/** Afficher l'état des cases de la grille. */
@@ -54,13 +54,18 @@ public class Puissance4 {
 		System.out.println("========");
 	}
 
-    /** Inserer un pion dans la grille. */
+    /**
+     * Inserer un pion dans la grille.
+     *
+     * @return TODO
+     */
 	public boolean insérerPion(int colonneChoisie) {
 		for (int j = Puissance4.NB_LIGNES - 1; j >= 0; j--) {
 			if (grille[j][colonneChoisie] == Pion.CASE_VIDE) {
-				grille[j][colonneChoisie] = this.joueurCourant.obtenirPion();
+				grille[j][colonneChoisie] = joueurCourant.obtenirPion();
 				return true;
 			}
+            /* TODO Expliquer ce cas particulier (parce qu'il est interessant pour la compréhension). */
 			if (j == 0) {
 				return false;
 			}
@@ -68,40 +73,31 @@ public class Puissance4 {
 		return false;
 	}
 	
-	/** Changement de joueur.*/
-	public void changerJoueur (){
-		if (joueurCourant == J1) {
-			joueurCourant = J2;
-		}
-		else if (joueurCourant == J2) { 
-			joueurCourant = J1;
-		}
+	/** Changement de joueur. */
+	public void joueurSuivant() {
+        /* TODO J'ai simplifié. Demandez en TP si vous ne comprenez pas. */
+        joueurCourant = joueurCourant.equals(joueur1) ? joueur2 : joueur1;
 	}
-	
-	/** Lancer le jeu. */
-	public void jouer() {
-		int colonneChoisie;
-		this.afficher();
-		while (true) {
-			Scanner sc = new Scanner(System.in);
-		colonneChoisie=sc.nextInt();
-		if ((colonneChoisie > 0) && (colonneChoisie <= 7)) {
-            if (!this.insérerPion(colonneChoisie-1)) {
-            	System.err.println("Colonne pleine");
+
+    /** Lancer le jeu. */
+    public void jouer() {
+        this.afficher();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            int colonneChoisie = sc.nextInt();
+            if ((colonneChoisie > 0) && (colonneChoisie <= 7)) {
+                if (this.insérerPion(colonneChoisie - 1)) {
+                    // TODO Vérification de victoire : nécessitera certainement (pour être plus propre) de passer la grille dans une classe dédiée.
+                    joueurSuivant();
+                }
+                else {
+                    System.err.println("Colonne pleine");
+                }
             }
             else {
-            	// XXX Vérification de victoire
-            	
-            	
-            	
-            changerJoueur();
+                System.err.println("Valeur incorrecte.");
             }
-            
+            this.afficher();
         }
-        else {
-            System.err.println("Valeur incorrecte.");
-        }
-		this.afficher();
-		}
-	}
+    }
 }
